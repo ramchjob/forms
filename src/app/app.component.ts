ae2dc85ce0c6919861e119680219f698fd47439d
rchimata <rchimata@rythmos.com>
2019-11-17 10:44:08 -0600

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,9 @@ export class AppComponent implements OnInit {
   customers: Customer[] = JSON.parse('[{"id":"1", "name":"First Name"}, {"id":"2", "name":"First Name2"}, {"id":"3", "name":"First Name3"}]');
     customerColumns: string[] = [
         'id',
-        'name'
+        'name',
+        'phoneNumber'
+
     ];
     customerDataSource: MatTableDataSource<Customer>;
     @ViewChild(MatPaginator) paginator: MatPaginator;
